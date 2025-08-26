fab39f937ebd5bd3d88c22cc5d7b8c84ef607d17
unknown <samirelhadad28@gmail.com>
2025-08-26 20:58:09 +0300

    Refactor code structure for improved readability and maintainability; removed redundant code blocks and optimized function calls.

---
+++ Day01/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task_1
+
+            Console.WriteLine("Enter Your Age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Your Age is : {age}");
+
+            //Task_2
+
+            //String name = "Samir";
+            //Convert.ToInt32(name); // Invalid Cast Exception and crash the program
+            //Console.WriteLine($"Your Name is : {name}");
+
+            //Task_3
+
+            float num1 = 19.5f;
+            float num2 = 20.2f;
+            float result = num1 + num2;
+            Console.WriteLine($"The result is: {result}"); // The result is: 39.7
+
+            //Task_4
+
+            int number = 10;
+            int number1 = number;
+            number1 = 20; // modifying the value of number1
+            Console.WriteLine($"The number is: {number}"); // The number is: 20
+
+            //Task_5
+
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int[] numbers1 = numbers;
+            numbers1[0] = 10; // modifying the first element of numbers1
+            Console.WriteLine($"The first element is: {numbers[0]}"); // The first element is: 10
+
+
+            //Task_6
+
+            Console.WriteLine("Enter Your first Name  : ");
+            String firstName = Console.ReadLine();
+            Console.WriteLine("Enter Your last Name  : ");
+            String lastName = Console.ReadLine();
+            String fullName = firstName + " " + lastName;
+            Console.WriteLine($"Full Name is: {fullName}"); // Full Name is: Samir Elhadad
+
+            //Task_7
+            //b
+            //d
+            //d
+
+        }
+    }
+}
