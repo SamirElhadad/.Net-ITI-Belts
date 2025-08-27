5eb33f4c41625dd1201b586f965a7f4f587d853f
SamirElhadad <samirelhadad28@gmail.com>
2025-08-27 17:05:14 +0300

    a program to create a Simple Calculator using switch.

---
+++ Day02/ConsoleApp11/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task_11 Write a program to create a Simple Calculator using switch.
+
+                Console.Write("Enter First Number : ");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.Write("Enter Second Number : ");
+                int num2 = int.Parse(Console.ReadLine());
+                Console.Write("Enter Operator (+, -, *, /) : ");
+                char op = char.Parse(Console.ReadLine());
+                switch (op)
+                {
+                    case '+':
+                        Console.WriteLine($"The Result is = {num1 + num2}");
+                        break;
+                    case '-':
+
+                        Console.WriteLine($"The Result is = {num1 - num2}");
+                        break;
+                    case '*':
+                        Console.WriteLine($"The Result is = {num1 * num2}");
+                        break;
+                    case '/':
+                        if (num2 != 0)
+                        {
+                            Console.WriteLine($"The Result is = {num1 / num2}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: Division by zero is not allowed.");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Error: Invalid operator.");
+                        break;
+                }
+
+            #endregion
+        }
+    }
+}
