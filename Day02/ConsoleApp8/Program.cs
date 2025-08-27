f56e6d6566f30f5fe8751f1baa2e57a620ed6e78
SamirElhadad <samirelhadad28@gmail.com>
2025-08-27 16:58:17 +0300

    a program allows to user to insert number & print all even numbers between 1 to this number.

---
+++ Day02/ConsoleApp8/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task_8 Write a program that allows to user to insert number then print all even numbers between 1 to this number.
+
+                Console.Write("Enter your num: ");
+                int number7 = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= number7; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write($" {i} ");
+                    }
+                }
+
+            #endregion
+        }
+    }
+}
