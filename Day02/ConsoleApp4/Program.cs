758ae064420b802305323a56ddf80e4763fe16b6
SamirElhadad <samirelhadad28@gmail.com>
2025-08-27 16:46:17 +0300

    a program allows insert an integer number then check If a number is even or odd.

--- Day02/ConsoleApp4/Program.cs
+++ Day02/ConsoleApp4/Program.cs
@@ -7,8 +7,8 @@
             #region Task_4 Write a program that allows the user to insert an integer number then check If a number is even or odd.
 
             Console.Write("Enter your num: ");
-            int number = int.Parse(Console.ReadLine());
-            if (number % 2 == 0)
+            int number4 = int.Parse(Console.ReadLine());
+            if (number4 % 2 == 0)
             {
                 Console.WriteLine("Even");
             }
