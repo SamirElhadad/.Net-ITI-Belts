501a9f433cbaf3c5dd7c3eacde6a4d9abb400856
SamirElhadad <samirelhadad28@gmail.com>
2025-08-27 16:31:28 +0300

    program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.

---
+++ Day02/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Task_2 Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+
+                Console.Write("Enter your num: ");
+                int number = int.Parse(Console.ReadLine());
+                if (number > 0)
+                {
+                    Console.WriteLine("Positive");
+                }
+                else if (number < 0)
+                {
+                    Console.WriteLine("Negative");
+                }
+                else
+                {
+                    Console.WriteLine("Zero");
+                }
+
+            #endregion
+
+
+
+
+        }
+    }
+}
