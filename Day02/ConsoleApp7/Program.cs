22391d10a56de492851fd733b832be6d9992c31e
SamirElhadad <samirelhadad28@gmail.com>
2025-08-27 16:54:40 +0300

    a program print a multiplication table up to 12.

---
+++ Day02/ConsoleApp7/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task_7 Write a program that allows the user to insert an integer then print a multiplication table up to 12.
+
+                Console.Write("Enter your num: ");
+                int number = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= 12; i++)
+                {
+                    Console.Write($" {number * i} ");
+                }
+
+            #endregion
+        }
+    }
+}
