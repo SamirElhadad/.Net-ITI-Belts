aa28422e3a2e6b8d8ee3dcef65e28ee2ae365920
SamirElhadad <samirelhadad28@gmail.com>
2025-08-27 16:47:57 +0300

    a program that takes character from the user then if it is a vowel chars  print (vowel) otherwise print (consonant)

--- Day02/ConsoleApp5/Program.cs
+++ Day02/ConsoleApp5/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Task_5 Write a program that takes character from the user then if it is a vowel chars (a,e,I,o,u) then print (vowel) otherwise print (consonant).
+
+                Console.Write("Enter your char: ");
+                char ch = char.Parse(Console.ReadLine());
+                char[] vowelStr = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' }
+                ;
+                foreach (char i in vowelStr)
+                {
+                    if (ch == i)
+                    {
+                        Console.WriteLine("Vowel");
+                        return;
+                    }
+                }
+                Console.WriteLine("Consonant");
+            #endregion
         }
     }
 }
