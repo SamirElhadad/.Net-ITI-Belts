6cbabc43e6524322572180db81c785fe83c912ed
SamirElhadad <samirelhadad28@gmail.com>
2025-08-27 16:43:22 +0300

    a program that takes 3 integers from the user then prints the max element and the min element ( Without built-in functions ) .

---
+++ Day02/ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task_3 Write a program that takes 3 integers from the user then prints the max element and the min element.
+
+                int maxNum = 0;
+                int minNum = 0;
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write($" Enter Your {i + 1}: ");
+                    int number3 = int.Parse(Console.ReadLine());
+                    if (number3 > maxNum)
+                    {
+                        maxNum = number3;
+                    }
+                    else
+                    {
+                        minNum = number3;
+                    }
+                }
+                Console.WriteLine($"Max = {maxNum}");
+                Console.WriteLine($"Mix = {minNum}");
+
+            #endregion
+
+        }
+    }
+}
