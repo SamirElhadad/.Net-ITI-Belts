89e7c781fcecc5d0c72e8c5dc85803cf46f572cf
SamirElhadad <samirelhadad28@gmail.com>
2025-08-28 16:56:27 +0300

    Pizza Order System (Enum)

--- Phase_1-fundamentals/C#/Day03/ConsoleApp2/Program.cs
+++ Phase_1-fundamentals/C#/Day03/ConsoleApp2/Program.cs
@@ -1,10 +1,71 @@
-﻿namespace ConsoleApp2
+﻿using ConsoleApp1;
+
+namespace ConsoleApp2
 {
     internal class Program
     {
+        #region Enum of Task_2
+
+        enum operation
+        {
+            Deposit = 1,
+            Withdraw
+        }
+
+        #endregion
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Task_2 Encapsulation + Validation
+
+                BankAccount account = new BankAccount();
+
+                Console.Write("Enter Your Balance: ");
+                decimal initialBalance = Convert.ToDecimal(Console.ReadLine());
+                account.Balance = initialBalance;
+                if (account.Balance < 0)
+                {
+                    Console.WriteLine("Error: Balance cannot be negative");
+                    return;
+                }
+                Console.WriteLine("Select Operation: ");
+                Console.WriteLine("1) Deposit: ");
+                Console.WriteLine("2) Withdraw: ");
+
+                Console.Write("Enter Your Choice : ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                if (choice < 1 || choice > 2)
+                {
+                    Console.WriteLine("Invalid Choice");
+                    return;
+                }
+
+                Console.Write("Enter Amount : ");
+                decimal amount = Convert.ToDecimal(Console.ReadLine());
+                if (amount <= 0)
+                {
+                    Console.WriteLine("Error: Balance cannot be negative");
+                    return;
+                }
+
+                operation op = (operation)choice;
+                switch (op)
+                {
+                    case operation.Deposit:
+                        account.Deposit(amount);
+                        Console.WriteLine($"Deposited: ${amount}, New Balance: ${account.Balance}");
+                        break;
+                    case operation.Withdraw:
+                        if (amount > account.Balance)
+                        {
+                            Console.WriteLine("Error: Insufficient funds");
+                            return;
+                        }
+                        account.Withdraw(amount);
+                        Console.WriteLine($"Withdrew: ${amount}, New Balance: ${account.Balance}");
+                        break;
+                }
+
+            #endregion
         }
     }
 }
--- Phase_1-fundamentals/C#/Day03/ConsoleApp3/Program.cs
+++ Phase_1-fundamentals/C#/Day03/ConsoleApp3/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace ConsoleApp3
+﻿using ConsoleApp1;
+
+namespace ConsoleApp3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Task_3 Automatic Property
+
+                Student student = new Student();
+
+                Console.Write("Enter Your Id: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+                student.Id = id;
+
+                Console.Write("Enter Your Full Name: ");
+                string name = Console.ReadLine();
+                student.FullName = name;
+
+                Console.Write("Enter Your Grade: ");
+                double grade = Convert.ToDouble(Console.ReadLine());
+                student.Grade = grade;
+
+                student.DisplayInfo();
+
+            #endregion
         }
     }
 }
