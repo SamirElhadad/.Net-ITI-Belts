6b97efba3239515a78b7208299e85b000ce958df
SamirElhadad <samirelhadad28@gmail.com>
2025-08-30 11:47:20 +0300

    A class has:A static field TotalCars that counts how many cars are created. A readonly field ChassisNumber that is assigned only in the constructor.

--- Phase_1-fundamentals/C#/Day04/ConsoleApp1/Car.cs
+++ Phase_1-fundamentals/C#/Day04/ConsoleApp1/Car.cs
@@ -8,21 +8,21 @@ namespace ConsoleApp1
 {
     public class Car
     {
-        public static int _TotalCars;
-        public readonly int _ChassisNumber;
+        public static int TotalCars;
+        public readonly int ChassisNumber;
         public string Model { get; set; }
         public double Price { get; set; }
 
         public Car(int chassisNumber, string model, double price)
         {
-            _ChassisNumber = chassisNumber;
+            ChassisNumber = chassisNumber;
             Model = model;
             Price = price;
-            _TotalCars++;
+            TotalCars++;
         }
         public void DisplayInfo()
         {
-            Console.WriteLine($"Chassis Number: {_ChassisNumber}, Model: {Model}, Price: {Price}");
+            Console.WriteLine($"Chassis Number: {ChassisNumber}, Model: {Model}, Price: {Price}");
         }
     }
 }
