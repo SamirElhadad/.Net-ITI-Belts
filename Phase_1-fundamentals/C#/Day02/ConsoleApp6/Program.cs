3982e9e5ce3005d2c3bf66d69e1fb96665d6e164
SamirElhadad <samirelhadad28@gmail.com>
2025-08-31 02:09:24 +0300

    program that allows the user to insert an integer then print all numbers between 1 to that number.

--- Phase_1-fundamentals/C#/Day02/ConsoleApp6/Program.cs
+++ Phase_1-fundamentals/C#/Day02/ConsoleApp6/Program.cs
@@ -6,12 +6,14 @@
         {
             #region Task_6 Write a program that allows the user to insert an integer then print all numbers between 1 to that number.
 
+
                 Console.Write("Enter your num: ");
                 int number5 = int.Parse(Console.ReadLine());
                 for (int i = 1; i < number5; i++)
                 {
                     Console.Write($" {i} ");
                 }
+
 
             #endregion
         }
