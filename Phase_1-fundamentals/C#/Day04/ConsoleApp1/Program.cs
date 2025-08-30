882548ec1a12c6457d9ac63d391230fec980f8a0
SamirElhadad <samirelhadad28@gmail.com>
2025-08-30 11:44:11 +0300

    3 cars and display their details along with the total number of cars.

--- Phase_1-fundamentals/C#/Day04/ConsoleApp1/Program.cs
+++ Phase_1-fundamentals/C#/Day04/ConsoleApp1/Program.cs
@@ -5,13 +5,13 @@
         static void Main(string[] args)
         {
             
-            Car car1 = new Car(101 , "BMW" , 25000);
-            Car car2 = new Car(102 , "Porch", 30000);
-            Car car3 = new Car(103 , "Mercedes-Benz", 25000);
+            Car car0 = new Car(101 , "BMW" , 25000);
+            Car car1 = new Car(102 , "Porch", 30000);
+            Car car2 = new Car(103 , "Mercedes-Benz", 25000);
 
+            car0.DisplayInfo();
             car1.DisplayInfo();
             car2.DisplayInfo();
-            car3.DisplayInfo();
 
             Console.WriteLine($"Total Car is : {Car.TotalCars}");
 
