b13a8e3884c14bd746a51e660877b984983b14a9
SamirElhadad <samirelhadad28@gmail.com>
2025-08-31 15:10:02 +0300

    a property Email and override the method to show payment details.

--- Phase_1-fundamentals/C#/Day05/ConsoleApp1/PayPalPayment.cs
+++ Phase_1-fundamentals/C#/Day05/ConsoleApp1/PayPalPayment.cs
@@ -8,10 +8,12 @@ namespace ConsoleApp1
 {
     public class PayPalPayment : Payment
     {
+
         public string Email;
         public override void ProcessPayment()
         {
             Console.WriteLine($"Processing PayPal Payment of {Amount} {Currency} using Email: {Email}");
         }
+
     }
 }
