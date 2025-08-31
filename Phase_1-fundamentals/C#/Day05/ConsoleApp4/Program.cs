45deba06788e8c3786691871c465fc4794c19715
SamirElhadad <samirelhadad28@gmail.com>
2025-08-31 15:16:37 +0300

    two student objects: one with only a name, and one with both name and age. Display their details.

--- Phase_1-fundamentals/C#/Day05/ConsoleApp4/Program.cs
+++ Phase_1-fundamentals/C#/Day05/ConsoleApp4/Program.cs
@@ -4,11 +4,13 @@
     {
         static void Main(string[] args)
         {
+
             Student student1 = new Student("Ahmed");
             Student student2 = new Student("Samir", 20);
 
             student1.DisplayInfo(); 
             student2.DisplayInfo(); 
+
         }
     }
 }
