2c59a70454f260c83c1bd74ba5bff267cfdcda63
SamirElhadad <samirelhadad28@gmail.com>
2025-09-03 16:31:20 +0300

    Day01

---
+++ Phase_1-fundamentals/Advanced_C#/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new List<Student>
+            {
+                new Student("Samir", 20),
+                new Student("Amr", 9),
+                new Student("Mohamed", 14),
+                new Student("Ahmed")
+            };
+
+
+            student[0].AddScore(90, 80, 70);
+            student[1].AddScore(90, 90, 90);
+            student[2].AddScore(50, 60, 70);
+            student[3].AddScore(80, 80, 90);
+            
+
+            string scoreToAdd = "80";
+            if (int.TryParse(scoreToAdd, out int parsedScore))
+            {
+                student[2].AddScore(parsedScore);
+            }
+
+            Console.WriteLine("===> All Students Details <===");
+            foreach (var s in student)
+            { 
+                var studentDetails = new
+                {
+                    s.Name,
+                    s.Age,
+                    AverageScore = s.GetAverageScore()
+                };
+                Console.WriteLine($"Name: {studentDetails.Name}, Age: {studentDetails.Age?.ToString() ?? "Null"}, Average Score: {studentDetails.AverageScore?.ToString("F2") ?? "Null"}");
+            }
+
+
+            Console.WriteLine("\n===> High Achievers (Avg > 80) <===");
+            var highAchievers = student.Where(s => s.GetAverageScore() > 80);
+
+            foreach (var x in highAchievers)
+            {
+                Console.WriteLine($"Name: {x.Name}, Average Score: {x.GetAverageScore()?.ToString("F2")}");
+            }
+
+        }
+    }
+}
---
+++ Phase_1-fundamentals/Advanced_C#/ConsoleApp1/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int? Age { get; set; }
+        public List<int> Scores { get; set; }
+
+
+        public Student(string name, int? age = null)
+        {
+            Name = name;
+            Age = age;
+            Scores = new List<int>();
+
+        }
+
+        public void AddScore(params int[] scores)
+        {
+            Scores.AddRange(scores);
+        }
+        public double? GetAverageScore()
+        {
+
+            return Scores.Count == 0 ? null : Scores.Average();
+        }
+    }
+}
