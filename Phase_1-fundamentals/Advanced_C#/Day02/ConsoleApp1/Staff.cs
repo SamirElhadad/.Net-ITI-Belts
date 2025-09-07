b13ff39829e9b1249da6b7f835110a196d8f26d4
SamirElhadad <samirelhadad28@gmail.com>
2025-09-07 16:41:40 +0300

    child classe Staff inheriting from Employee.

--- Phase_1-fundamentals/Advanced_C#/Day02/ConsoleApp1/Staff.cs
+++ Phase_1-fundamentals/Advanced_C#/Day02/ConsoleApp1/Staff.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+
     public class Staff : Employee
     {
         public Staff(string name, double salary) : base(name, salary)
         {
         }
     }
+
 }
