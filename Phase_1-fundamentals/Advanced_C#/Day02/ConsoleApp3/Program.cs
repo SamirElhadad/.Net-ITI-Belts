bb3538b9b73b69ff5aaafa86508e3a46635348b0
SamirElhadad <samirelhadad28@gmail.com>
2025-09-09 00:28:17 +0300

    List Operations

---
+++ Phase_1-fundamentals/Advanced_C#/Day02/ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> students = new List<string> { "Ali", "Sara", "Mona", "Abdullah", "Mohamed" };
+            students.Add("Laila");
+            students.Add("Omar");
+            students.Add("Ali");
+            students.Remove("Mona");
+            Console.WriteLine("All Student Names:");
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Names Starting with 'A':");
+            foreach (var student in students)
+            {
+                if (student.StartsWith("A"))
+                {
+                    Console.WriteLine(student);
+                }
+            }
+        }
+    }
+}
