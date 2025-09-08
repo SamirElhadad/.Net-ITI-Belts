e67da7a06979d9017c628cb81e698b2982b62716
SamirElhadad <samirelhadad28@gmail.com>
2025-09-08 20:40:15 +0300

    Local Function Calculator

---
+++ Phase_1-fundamentals/Advanced_C#/Day02/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Threading;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 10, 15, 20 };
+            void Calculate()
+                {
+                int GetSum()
+                {
+                    int sum = 0;
+                    foreach (var num in numbers)
+                    {
+                        sum += num;
+                    }
+                    return sum;
+                }
+                int sum = GetSum();
+                double average = (double)sum / numbers.Length;
+                Console.WriteLine($"Sum: {sum}, Average: {average}");
+            }
+            Calculate();
+        }
+    }
+}
