a4b56c694f17f602e62abfae67852fba32e37478
SamirElhadad <samirelhadad28@gmail.com>
2025-09-11 11:14:16 +0300

    Dictionary Lookup

---
+++ Phase_1-fundamentals/Advanced_C#/Day02/ConsoleApp4/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string , string> country = new Dictionary<string , string>()
+            {
+                {"EG" , "Egypt" },
+                {"KSA" , "Saudi Arabia" },
+                {"USA" , "United States" },
+                {"UK" , "United Kingdom" },
+                {"UAE" , "United Arab Emirates" }
+            };
+
+            Console.Write($"Enter Code : ");
+            string code = Console.ReadLine().ToUpper();
+
+            if (country.ContainsKey(code))
+            {
+                Console.WriteLine($"Country Name Is : {country[code]}");
+            }
+            else
+            {
+                Console.WriteLine("Code Not Found");
+            }
+        }
+    }
+}
