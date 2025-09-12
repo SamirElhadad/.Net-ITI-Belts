c1b4c72cc4e62bb4e5023f8fa2e55a8356ef636e
SamirElhadad <samirelhadad28@gmail.com>
2025-09-12 14:32:42 +0300

    Passing by Value with Reference Type

---
+++ Phase_1-fundamentals/Advanced_C#/Day02/ConsoleApp5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Threading.Channels;
+using System.Xml.Linq;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = { 1 , 2 , 3 };
+            Console.WriteLine("Array before calling ModifyArray(): " + string.Join(", ", numbers));
+            void ModifyArray(int[] arr)
+            {
+                arr[0] = 100; 
+                arr = new int[] { 9, 9, 9 }; 
+            }
+            ModifyArray(numbers);
+            Console.WriteLine("Array after calling ModifyArray(): " + string.Join(", ", numbers));
+
+
+        }
+    }
+}
