29e07e963b96b87e92a1533eb49e783b5b0863a9
SamirElhadad <samirelhadad28@gmail.com>
2025-09-13 22:40:36 +0300

    A class with method CountCharacters() that returns the number of characters in the string.

---
+++ Phase_1-fundamentals/Advanced_C#/Day03/ConsoleApp1/CountChar_Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class CountChar_Extensions
+    {
+        public static int CountCharacters(this string text)
+        {
+            if (string.IsNullOrEmpty(text)) return 0;
+            return text.Length;
+        }
+    }
+}
