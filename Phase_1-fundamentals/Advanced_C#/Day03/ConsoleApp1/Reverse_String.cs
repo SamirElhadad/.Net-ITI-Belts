297e890c399794bd493ed20df0a8bc22b16fd430
SamirElhadad <samirelhadad28@gmail.com>
2025-09-13 22:42:44 +0300

    a class with method ReverseText() that returns the string in reverse order.

---
+++ Phase_1-fundamentals/Advanced_C#/Day03/ConsoleApp1/Reverse_String.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Reverse_String
+    {
+        public static string ReverseText(this string text)
+        {
+            if (string.IsNullOrEmpty(text)) return string.Empty;
+            return new string(text.Reverse().ToArray());
+        }
+    }
+}
