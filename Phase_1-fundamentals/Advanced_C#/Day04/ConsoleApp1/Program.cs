315b2403b28a562584c387b4e08d60c878081c44
SamirElhadad <samirelhadad28@gmail.com>
2025-09-15 23:38:55 +0300

    Day04

---
+++ Phase_1-fundamentals/Advanced_C#/Day04/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.Write($"Enter Your First Number : ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write($"Enter Your Second Number : ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                int result = num1 / num2;
+                Console.WriteLine($"The Result is : {result}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Error: Division by zero is not allowed.");
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. Please enter numeric values.");
+            }
+
+            finally
+            {
+                Console.WriteLine("Program finished.");
+            }
+        }
+    }
+}
