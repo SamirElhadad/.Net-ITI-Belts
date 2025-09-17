e822b45c967ffc648b36f151d7ad69b15f225097
SamirElhadad <samirelhadad28@gmail.com>
2025-09-17 11:39:33 +0300

    Day04

---
+++ Phase_1-fundamentals/Advanced_C#/Day04/ConsoleApp2/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Employee : IComparable<Employee>
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public DateTime HiringDate { get; set; }
+
+        public int CompareTo(Employee other)
+        {
+            int res = this.HiringDate.Year.CompareTo(other.HiringDate.Year);
+            if (res == 0)
+            {
+              res = this.HiringDate.Month.CompareTo(other.HiringDate.Month);
+            }
+            if (res == 0)
+            {
+              res = this.HiringDate.Day.CompareTo(other.HiringDate.Day);
+            }
+            return res;
+
+
+        }
+
+        
+    }
+}
---
+++ Phase_1-fundamentals/Advanced_C#/Day04/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = new List<Employee>
+            {
+                new Employee { Name = "Samir"   , Salary = 60000 , HiringDate = new DateTime(2020, 1, 15) },
+                new Employee { Name = "Mostafa" , Salary = 55000 , HiringDate = new DateTime(2019, 3, 10) },
+                new Employee { Name = "Ahmed"   , Salary = 70000 , HiringDate = new DateTime(2021, 6, 5) }
+            };
+            employees.Sort();
+            foreach (var emp in employees)
+            {
+                Console.WriteLine($"{emp.Name}, {emp.Salary}, {emp.HiringDate.ToShortDateString()}");
+            }
+
+        }
+    }
+}
