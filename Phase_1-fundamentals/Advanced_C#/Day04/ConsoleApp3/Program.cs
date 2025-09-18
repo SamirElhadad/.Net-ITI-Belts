72693ddac41704c6a74f438d74dbab113fc0f472
SamirElhadad <samirelhadad28@gmail.com>
2025-09-18 06:34:20 +0300

    Day04

---
+++ Phase_1-fundamentals/Advanced_C#/Day04/ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var logger = new FileLogger("log.txt"))
+            {
+                logger.LogMessage("First log message");
+                logger.LogMessage("Second log message");
+                logger.LogMessage("Third log message");
+            }
+
+            Console.WriteLine("Logs written to file.");
+        }
+    }
+}
